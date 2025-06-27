7dcf0cd9282e1a9511473bf7c4bd730f3958ae5c
Jammajo <jairogamarran@hotmail.com>
2025-06-27 01:33:51 -0500

    Update main.rs

--- lemon/lemon_cli/src/main.rs
+++ lemon/lemon_cli/src/main.rs
@@ -1,3 +1,9 @@
+use lemon_core::interpret;
+
 fn main() {
-    println!("Hello, world!");
+    let input = "print(add 2,3)";
+    match interpret(input) {
+        Ok(result) => println!("Tokens: {}", result),
+        Err(e) => eprintln!("Error: {}", e),
+    }
 }

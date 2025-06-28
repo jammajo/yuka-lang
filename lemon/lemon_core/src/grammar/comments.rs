ba55d231a807bce1f4e077fef8d02a25ecf3dd7c
Jammajo <jairogamarran@hotmail.com>
2025-06-28 10:38:33 -0500

    Create comments.rs

---
+++ lemon/lemon_core/src/grammar/comments.rs
@@ -0,0 +1,16 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum Comment {
+    LineComment,
+    BlockCommentOpen,
+    BlockCommentClose,
+}
+impl Comment {
+    pub fn from_str(s: &str) -> Option<Comment> {
+        match s {
+            "//" => Some(Comment::LineComment),
+            "/*" => Some(Comment::BlockCommentOpen),
+            "*/" => Some(Comment::BlockCommentClose),
+            _ => None,
+        }
+    }
+}

bafa3225f53461f3f7cf750404042bbb55e013a2
Jammajo <jairogamarran@hotmail.com>
2025-06-28 10:37:51 -0500

    Create logicals.rs

---
+++ lemon/lemon_core/src/grammar/logicals.rs
@@ -0,0 +1,20 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum Logical {
+    And,
+    Or,
+    Not,
+}
+
+impl Logical {
+    pub fn from_str(s: &str) -> Option<Logical> {
+        match s.to_lowercase().as_str() {
+            "and" => Some(Logical::And),
+            "&&" => Some(Logical::And),
+            "or" => Some(Logical::Or),
+            "||" => Some(Logical::Or),
+            "not" => Some(Logical::Not),
+            "!" => Some(Logical::Not),
+            _ => None,
+        }
+    }
+}
